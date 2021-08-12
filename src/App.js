1ae6371033394375bbdfd803d9a8ddab4fae787d
chaimaB <chaima.braiek@esprit.tn>
2021-08-12 04:49:51 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Home from "./components/Home";
+import React from "react";
+
+function App() {
+  return (
+      <div className="App">
+          <Home/>
+          <footer>
+              <div>Copyright</div>
+          </footer>
+      </div>
+  );
+}
+
+export default App;
